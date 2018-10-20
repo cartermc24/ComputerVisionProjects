bb5a334c28ae1af68ae98db0da7605c1c2bb00d2
Carter McCardwell <carter@mccardwell.net>
2018-10-20 15:44:11 -0400

    Harris and ncorr work

--- Project-2-Mosaicing/main.cpp
+++ Project-2-Mosaicing/main.cpp
@@ -3,8 +3,10 @@
 #include "utils/FrameReader.h"
 #include "utils/ImageShower.h"
 #include <unistd.h>
+#include <tuple>
+#include <algorithm>
 
-cv::Mat get_normalized_correlation(cv::Mat F, cv::Mat G);
+std::vector<std::tuple<cv::Point, cv::Point, double_t>> get_normalized_correlation(cv::Mat F, cv::Mat G, uint8_t window_size);
 cv::Mat harris_corner_detector(cv::Mat img, uint8_t window_size);
 
 int main(int argc, char **argv) {
@@ -27,31 +29,52 @@ int main(int argc, char **argv) {
     shower.showImage(corrnerdHarri);
     */
 
-    cv::Mat output = harris_corner_detector(reader.getNextFrame(), 3);
+    ImageShower frame1("F");
+    ImageShower frame2("G");
 
-    std::cout << output << std::endl;
+    cv::Mat scaled1, scaled2;
+    cv::resize(reader.getNextFrame(), scaled1, cv::Size(), 0.25, 0.25);
+    cv::resize(reader.getNextFrame(), scaled2, cv::Size(), 0.25, 0.25);
+
+    std::cout << "Running harris on image 1" << std::endl;
+    cv::Mat image1 = harris_corner_detector(scaled1, 3);
+    std::cout << "Running harris on image 2" << std::endl;
+    cv::Mat image2 = harris_corner_detector(scaled2, 3);
+
+    std::cout << "Running ncorr on image 1 & 2" << std::endl;
+    std::vector<std::tuple<cv::Point, cv::Point, double_t>> corr_pts = get_normalized_correlation(image1, image2, 3);
+
+    frame1.showImage(image1);
+    frame2.showImage(image2);
+
+    for (auto corr : corr_pts) {
+        std::cout << "-> Ncorr: F: " << std::get<0>(corr) << " G:" << std::get<1>(corr) << " Val: " << std::get<2>(corr) << std::endl;
+    }
+
+
 
     return 0;
 }
 
-cv::Mat get_normalized_correlation(cv::Mat F, cv::Mat G) {
-    auto g_anchor_pt = (uint8_t)floor(G.rows/2);
+std::vector<std::tuple<cv::Point, cv::Point, double_t>> get_normalized_correlation(cv::Mat F, cv::Mat G, uint8_t window_size) {
+    auto anchor_pt = (uint8_t)floor(window_size/2);
 
     // ------------- Find Normalized Correlation between f and g ----------------
     // Find Fhat and Ghat
     cv::Mat Fhat(F.rows, F.cols, CV_64FC1, cv::Scalar(0));
     cv::Mat Ghat(G.rows, G.cols, CV_64FC1, cv::Scalar(0));
     // Fhat
-    for (uint32_t x = g_anchor_pt; x < F.rows-g_anchor_pt; x++) {
-        for (uint32_t y = g_anchor_pt; y < F.cols-g_anchor_pt; y++) {
-            // Run "corr convolution" over F
-            uint32_t f_x_start = x - g_anchor_pt;
-            uint32_t f_y_start = y - g_anchor_pt;
+    std::cout << "\t-> Creating Fhat for ncorr" << std::endl;
+#pragma omp parallel for
+    for (uint32_t x = anchor_pt; x < F.rows-anchor_pt; x++) {
+        for (uint32_t y = anchor_pt; y < F.cols-anchor_pt; y++) {
+            uint32_t f_x_start = x - anchor_pt;
+            uint32_t f_y_start = y - anchor_pt;
 
             double_t fhat_val = 0;
-            for (uint32_t g_x = 0; g_x < G.rows; g_x++) {
-                for (uint32_t g_y = 0; g_y < G.cols; g_y++) {
-                    fhat_val += pow(F.at<uint8_t>(f_x_start+g_x, f_y_start+g_y), 2);
+            for (uint32_t f_x = 0; f_x < F.rows; f_x++) {
+                for (uint32_t f_y = 0; f_y < F.cols; f_y++) {
+                    fhat_val += pow(F.at<uint8_t>(f_x_start+f_x, f_y_start+f_y), 2);
                 }
             }
 
@@ -64,39 +87,75 @@ cv::Mat get_normalized_correlation(cv::Mat F, cv::Mat G) {
     }
 
     // Ghat
-    double ghat_val = 0;
-    for (uint32_t g_x = 0; g_x < G.rows; g_x++) {
-        for (uint32_t g_y = 0; g_y < G.cols; g_y++) {
-            ghat_val += pow(G.at<uint8_t>(g_x, g_y), 2);
-        }
-    }
-    ghat_val = sqrt(ghat_val);
-    for (uint32_t g_x = 0; g_x < G.rows; g_x++) {
-        for (uint32_t g_y = 0; g_y < G.cols; g_y++) {
-            Ghat.at<double_t>(g_x, g_y) = G.at<uint8_t>(g_x, g_y) / ghat_val;
-        }
-    }
+    std::cout << "\t-> Creating Ghat for ncorr" << std::endl;
+#pragma omp parallel for
+    for (uint32_t x = anchor_pt; x < G.rows-anchor_pt; x++) {
+        for (uint32_t y = anchor_pt; y < G.cols-anchor_pt; y++) {
+            uint32_t g_x_start = x - anchor_pt;
+            uint32_t g_y_start = y - anchor_pt;
 
-    cv::Mat ncorr(F.rows, F.cols, CV_64FC1, cv::Scalar(0));
-    // Ignore borders
-    for (uint32_t x = g_anchor_pt; x < F.rows-g_anchor_pt; x++) {
-        for (uint32_t y = g_anchor_pt; y < F.cols-g_anchor_pt; y++) {
-            // Run "corr convolution" over F
-            uint32_t f_x_start = x - g_anchor_pt;
-            uint32_t f_y_start = y - g_anchor_pt;
-
-            double_t ncorr_val = 0;
+            double_t ghat_val = 0;
             for (uint32_t g_x = 0; g_x < G.rows; g_x++) {
                 for (uint32_t g_y = 0; g_y < G.cols; g_y++) {
-                    ncorr_val += Fhat.at<double_t>(f_x_start+g_x, f_y_start+g_y) \
-                               * Ghat.at<double_t>(g_x, g_y);
+                    ghat_val += pow(G.at<uint8_t>(g_x_start+g_x, g_y_start+g_y), 2);
                 }
             }
 
-            ncorr.at<double_t>(x, y) = ncorr_val;
+            if (ghat_val > 0) {
+                Ghat.at<double_t>(x, y) = G.at<uint8_t>(x, y) / sqrt(ghat_val);
+            } else {
+                Ghat.at<double_t>(x, y) = 0;
+            }
         }
     }
-    return ncorr;
+
+    std::vector<std::tuple<cv::Point, cv::Point, double_t>> corr_points;
+    // Ignore borders
+    // Iterate through all points of G
+    std::cout << "\t-> Starting ncorr scan" << std::endl;
+    for (uint32_t g_x = anchor_pt; g_x < Ghat.rows-anchor_pt; g_x++) {
+        for (uint32_t g_y = anchor_pt; g_y < Ghat.cols-anchor_pt; g_y++) {
+            cv::Mat g_region(Ghat, cv::Rect(g_y-anchor_pt, g_x-anchor_pt, window_size, window_size));
+
+            double_t max_ncorr_val = 0;
+            uint32_t max_f_x = 0, max_f_y = 0;
+
+            // Iterate through all points of F
+            for (uint32_t f_x = anchor_pt; f_x < Fhat.rows - anchor_pt; f_x++) {
+                for (uint32_t f_y = anchor_pt; f_y < Fhat.cols - anchor_pt; f_y++) {
+
+                    uint32_t f_x_start = f_x - anchor_pt;
+                    uint32_t f_y_start = f_y - anchor_pt;
+
+                    // Do the ncorr operation on R[g] and R[f]
+                    double_t ncorr_val = 0;
+                    for (uint32_t scan_x = 0; scan_x < g_region.rows; scan_x++) {
+                        for (uint32_t scan_y = 0; scan_y < g_region.cols; scan_y++) {
+                            ncorr_val += Fhat.at<double_t>(f_x_start + scan_x, f_y_start + scan_y) \
+                                       * g_region.at<double_t>(scan_x, scan_y);
+                        }
+                    }
+
+                    if (ncorr_val > max_ncorr_val) {
+                        max_f_x = f_x;
+                        max_f_y = f_y;
+                        max_ncorr_val = ncorr_val;
+                    }
+                }
+            }
+
+            if (max_ncorr_val > 0) {
+                cv::Point f_point(max_f_x, max_f_y);
+                cv::Point g_point(g_x, g_y);
+
+                corr_points.emplace_back(f_point, g_point, max_ncorr_val);
+            }
+        }
+    }
+
+
+    std::cout << "\t-> Found num ncorrs: " << corr_points.size() << std::endl;
+    return corr_points;
 }
 
 cv::Mat harris_corner_detector(cv::Mat img, uint8_t window_size) {
