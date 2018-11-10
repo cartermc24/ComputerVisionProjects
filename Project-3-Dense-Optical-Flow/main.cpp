653c36638ca6d9104e7329bbcd7a29e592213016
Carter McCardwell <cartermc24@gmail.com>
2018-11-10 02:51:52 -0500

    Bugfixes for smoothing and cv points

--- Project-3-Dense-Optical-Flow/main.cpp
+++ Project-3-Dense-Optical-Flow/main.cpp
@@ -27,7 +27,8 @@ int main(int argc, char **argv) {
     cv::Mat i2 = reader.getNextFrame();
 
     std::pair<cv::Mat, cv::Mat> flow_vectors = lukas_kanade(i1, i2);
-    display_optical_flow(i1, flow_vectors.first, flow_vectors.second);
+    shower1.showImage(i1);
+    display_optical_flow(i2, flow_vectors.first, flow_vectors.second);
     display_optical_flow_hsv(i1, flow_vectors.first, flow_vectors.second);
 
     return 0;
@@ -35,7 +36,7 @@ int main(int argc, char **argv) {
 
 void display_optical_flow(const cv::Mat &image, const cv::Mat &u_vectors, const cv::Mat &v_vectors) {
     // <<< Equivalent of the Matlab quiver function >>>
-    double_t QUIVER_THRESHOLD = 0.8;
+    double_t QUIVER_THRESHOLD = 0;
 
     cv::Mat dsp_image = image.clone();
     for (uint32_t row = 0; row < image.rows; row += 5) {
@@ -44,9 +45,9 @@ void display_optical_flow(const cv::Mat &image, const cv::Mat &u_vectors, const 
                 continue;
             }
 
-            cv::Point src_point(row, col);
-            cv::Point dst_point((int)std::round(row+u_vectors.at<double_t>(row, col)),
-                                (int)std::round(col+v_vectors.at<double_t>(row, col)));
+            cv::Point src_point(col, row);
+            cv::Point dst_point((int)std::round(col+v_vectors.at<double_t>(row, col)),
+                                (int)std::round(row+u_vectors.at<double_t>(row, col)));
 
             cv::arrowedLine(dsp_image, src_point, dst_point, cv::Scalar(255, 0, 0), 1);
         }
@@ -65,7 +66,7 @@ void display_optical_flow_hsv(const cv::Mat &image, const cv::Mat &u_vectors, co
             double_t vec_length = sqrt(pow(u_vectors.at<double_t>(row, col), 2) + pow(v_vectors.at<double_t>(row, col), 2));
             double_t angle = atan2(v_vectors.at<double_t>(row, col), u_vectors.at<double_t>(row, col)) + 3.14159;
 
-            printf("[%i,%i]: Angle: %f, length: %f\n", row, col, angle, vec_length);
+            //printf("[%i,%i]: Angle: %f, length: %f\n", row, col, angle, vec_length);
 
             hsv_img.at<cv::Vec3b>(row, col)[0] = (uint8_t)((angle) * (180/3.14159/2));
             hsv_img.at<cv::Vec3b>(row, col)[1] = (uint8_t)std::min<double_t>(vec_length*4, 255);
@@ -90,13 +91,9 @@ std::pair<cv::Mat, cv::Mat> lukas_kanade(const cv::Mat &image1, const cv::Mat &i
     cv::Mat smooth1, smooth2;
     cv::GaussianBlur(grayImg1, smooth1, cv::Size(), SMOOTHING_SIGMA);
     cv::GaussianBlur(grayImg2, smooth2, cv::Size(), SMOOTHING_SIGMA);
-    grayImg1 = smooth1;
-    grayImg2 = smooth2;
 
     // Get spacial intensity gradients for both images in x and y directions
-    cv::Mat space_int_1_x, space_int_1_y, space_int_2_x, space_int_2_y;
-    cv::Sobel(grayImg1, space_int_1_x, CV_64F, 1, 0);
-    cv::Sobel(grayImg1, space_int_1_y, CV_64F, 0, 1);
+    cv::Mat space_int_2_x, space_int_2_y;
     cv::Sobel(grayImg2, space_int_2_x, CV_64F, 1, 0);
     cv::Sobel(grayImg2, space_int_2_y, CV_64F, 0, 1);
 
@@ -104,7 +101,7 @@ std::pair<cv::Mat, cv::Mat> lukas_kanade(const cv::Mat &image1, const cv::Mat &i
     cv::Mat temporal_deriv(image1.rows, image2.cols, CV_64FC1, cv::Scalar(0));
     for (int row = 0; row < image1.rows; row++) {
         for (int cols = 0; cols < image1.cols; cols++) {
-            temporal_deriv.at<double_t>(row, cols) = grayImg2.at<double_t>(row, cols) - grayImg1.at<double_t>(row, cols);
+            temporal_deriv.at<double_t>(row, cols) = smooth2.at<double_t>(row, cols) - smooth1.at<double_t>(row, cols);
         }
     }
 
@@ -118,6 +115,7 @@ std::pair<cv::Mat, cv::Mat> lukas_kanade(const cv::Mat &image1, const cv::Mat &i
             IxIt(image1.rows, image2.cols, CV_64FC1, cv::Scalar(0)),
             IyIt(image1.rows, image2.cols, CV_64FC1, cv::Scalar(0));
 
+#pragma omp parallel for
     for (uint32_t row = anchor_pt; row < image1.rows-anchor_pt; row++) {
         for (uint32_t col = anchor_pt; col < image1.cols - anchor_pt; col++) {
             uint32_t f_row_start = row - anchor_pt;
@@ -151,12 +149,13 @@ std::pair<cv::Mat, cv::Mat> lukas_kanade(const cv::Mat &image1, const cv::Mat &i
     }
 
     // Solve for flow vectors u and v
-    cv::Mat u_flow_vector(image1.rows, image2.cols, CV_64FC1, cv::Scalar(0)),
-            v_flow_vector(image1.rows, image2.cols, CV_64FC1, cv::Scalar(0));
+    cv::Mat u_flow_vector(image1.rows, image1.cols, CV_64FC1, cv::Scalar(0)),
+            v_flow_vector(image1.rows, image1.cols, CV_64FC1, cv::Scalar(0));
     cv::Mat l_matrix(2, 2, CV_64FC1),
             r_matrix(2, 1, CV_64FC1);
 
-    for (uint32_t row = anchor_pt; row < image1.rows-anchor_pt; row++) {
+#pragma omp parallel for
+    for (uint32_t row = anchor_pt; row < image1.rows - anchor_pt; row++) {
         for (uint32_t col = anchor_pt; col < image1.cols - anchor_pt; col++) {
             l_matrix.at<double_t>(0, 0) = Ix2.at<double_t>(row, col);
             l_matrix.at<double_t>(0, 1) = IxIy.at<double_t>(row, col);
@@ -166,7 +165,7 @@ std::pair<cv::Mat, cv::Mat> lukas_kanade(const cv::Mat &image1, const cv::Mat &i
             r_matrix.at<double_t>(0, 0) = -IxIt.at<double_t>(row, col);
             r_matrix.at<double_t>(1, 0) = -IyIt.at<double_t>(row, col);
 
-            cv::Mat l_matrix_inv = l_matrix.inv(cv::DECOMP_SVD);
+            cv::Mat l_matrix_inv = l_matrix.inv();
             cv::Mat uv_mat = l_matrix_inv * r_matrix;
 
             u_flow_vector.at<double_t>(row, col) = uv_mat.at<double_t>(0, 0);
